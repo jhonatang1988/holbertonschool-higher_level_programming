96281c479683e427ec613322025713382909662d
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-09-17 17:10:27 +0000

    6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if (len(matrix) == 0):
+        return None
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{}".format(matrix[i][j]), end="")
+            if j != len(matrix[i]) - 1:
+                print("", end=" ")
+        print()
