bee51b1d06c839b4e3c40ba80732ee7387bb2a56
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-10-08 23:15:19 +0000

    5

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+class Rectangle:
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.__width + self.height)
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ("")
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            if not i == self.height - 1:
+                print()
+        return("")
+
+    def __repr__(self):
+        return ("Rectangle(" + str(self.__width) + ", " +
+                str(self.__height) + ")")
+
+    @staticmethod
+    def __del__():
+        print("Bye rectangle...")
