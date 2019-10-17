d17239fd3d753a9db260e3331a2918ea8b90d7b0
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-10-17 15:48:57 +0000

    1

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,6 +1,6 @@
 def number_of_lines(filename=""):
     lines = 0
-    with open(filename, encoding='utf-8') as a_file:
+    with open(filename) as a_file:
         for line in a_file:
             lines += 1
     return lines
