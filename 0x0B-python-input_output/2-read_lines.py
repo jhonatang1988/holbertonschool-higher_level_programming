185022ccd8aaa10643fb46c0ac3eedbfa0ec73f6
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-10-17 15:52:51 +0000

    2

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
+
 def read_lines(filename="", nb_lines=0):
-    with open('my_file_0.txt', encoding='utf-8') as a_file:
+    with open(filename, encoding='utf-8') as a_file:
         for lines, l in enumerate(a_file):
             if lines < nb_lines or nb_lines <= 0:
                 print("{}".format(l), end="")
