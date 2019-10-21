d0eca8cdc802341226523ed8c94aa25877965120
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-10-21 22:02:18 +0000

    16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,7 +11,18 @@ class Base():
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
-        if not list_dictionaries is None:
+        if not list_dictionaries is None and list_dictionaries:
             return json.dumps(list_dictionaries)
         return "[]"
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        a_list = []
+        for obj in list_objs:
+            a_list.append(obj.to_dictionary())
+        json_dictionary = Base.to_json_string(a_list)
+        filename = cls.__name__ + ".json"
+        with open(filename, encoding='utf-8', mode='w') as a_file:
+            return a_file.write(json_dictionary)
