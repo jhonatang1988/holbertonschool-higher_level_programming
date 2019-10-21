58eed77dabeba323c1bd8bda35e34543c5491086
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-10-21 20:42:09 +0000

    11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,3 +19,14 @@ class Square(Rectangle):
     def size(self, a_value):
         Rectangle.width.fset(self, a_value)
         Rectangle.height.fset(self, a_value)
+
+    def update(self, *args, **kwargs):
+        if args:
+            arg_names = list(self.__init__.__code__.co_varnames)
+            del(arg_names[0])
+            arg_names.insert(0, arg_names.pop())
+            for index, value in enumerate(args):
+                setattr(self, arg_names[index], value)
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
