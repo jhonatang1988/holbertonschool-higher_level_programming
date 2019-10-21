d6fd585934bc78fe015f427e0c013b31cd233672
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-10-21 16:51:19 +0000

    2, 3, 4, 5

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,43 +1,78 @@
 #!/usr/bin/python3
 from models.base import Base
 
+
 class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
 
     @property
     def width(self):
-        return self.__width
+        return (self.__width)
 
     @width.setter
-    def width(self):
-        self.__width = width
+    def width(self, a_value):
+        if type(a_value) is type(1):
+            if a_value < 1:
+                raise ValueError("width must be > 0")
+            else:
+                self.__width = a_value
+        else:
+            raise TypeError("width must be an integer")
+
 
     @property
     def height(self):
         return self.__height
 
     @height.setter
-    def height(self):
-        self.__height = height
+    def height(self, a_value):
+        if type(a_value) is type(1):
+            if a_value < 1:
+                raise ValueError("height must be > 0")
+            else:
+                self.__height = a_value
+        else:
+            raise TypeError("height must be an integer")
 
     @property
     def x(self):
         return self.__x
 
     @x.setter
-    def x(self):
-        self.__x = x
+    def x(self, a_value):
+        if type(a_value) is type(1):
+            if a_value < 0:
+                raise ValueError("x must be >= 0")
+            else:
+                self.__x = a_value
+        else:
+            raise TypeError("x must be an integer")
 
     @property
     def y(self):
         return self.__y
 
     @y.setter
-    def y(self):
-        self.__y = y
+    def y(self, a_value):
+        if type(a_value) is type(1):
+            if a_value < 0:
+                raise ValueError("y must be >= 0")
+            else:
+                self.__y = a_value
+        else:
+            raise TypeError("y must be an integer")
+
+    def area(self):
+        return self.__width * self.__height
+
+    def display(self):
+        for a_height in range(self.height):
+            for a_width in range(self.width):
+                print("#", end="")
+            print()
