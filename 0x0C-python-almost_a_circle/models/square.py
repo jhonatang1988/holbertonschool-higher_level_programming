e6e53ab42a47c37187b37493fce74af2bb81c8f5
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-10-22 20:39:29 +0000

    square model with comments in all methods

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,8 +2,6 @@
 """
 Square Model
 """
-
-
 from models.rectangle import Rectangle
 
 
@@ -12,23 +10,38 @@ class Square(Rectangle):
     Square Class
     """
     def __init__(self, size, x=0, y=0, id=None):
+        """
+        __init__ method
+        """
         self.size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """
+        __str__ method
+        """
         return ("[Square] ({:d}) {:d}/{:d} - {:d}"
                 .format(self.id, self.x, self.y, self.width))
 
     @property
     def size(self):
+        """
+        size getter
+        """
         return self.width
 
     @size.setter
     def size(self, a_value):
+        """
+        size setter
+        """
         Rectangle.width.fset(self, a_value)
         Rectangle.height.fset(self, a_value)
 
     def update(self, *args, **kwargs):
+        """
+        update method
+        """
         if args:
             arg_names = list(self.__init__.__code__.co_varnames)
             del(arg_names[0])
@@ -40,6 +53,9 @@ class Square(Rectangle):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """
+        to_dictionary method
+        """
         arg_names = list(self.__init__.__code__.co_varnames)
         del(arg_names[0])
         new_dict = {}
