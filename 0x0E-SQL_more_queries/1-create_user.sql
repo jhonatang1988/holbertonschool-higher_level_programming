e6f6172fbc11165516b439e79bf9af7d93c0012c
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-11-26 19:28:11 +0000

    1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,8 @@
 -- create user and grant of permissions
 CREATE USER IF NOT EXISTS
-       'user_0d_1'@'localhost'
-IDENTIFIED BY
-	   'user_0d_1_pwd';
+       'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
 GRANT PROXY
 ON root
 TO user_0d_1@localhost;
+FLUSH PRIVILEGES;
