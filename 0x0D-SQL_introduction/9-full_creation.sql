52337092e1ebfeaf3dd53e4101119cea0f831de7
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-11-26 00:54:49 +0000

    9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,9 @@
 -- creates a table
 -- fill description of table
 -- add records
-CREATE TABLE IF NOT EXISTS second_table (id INT);
-ALTER TABLE second_table ADD name VARCHAR(256);
-ALTER TABLE second_table ADD score INT;
+CREATE TABLE IF NOT EXISTS second_table (id INT,
+        name VARCHAR(256),
+	score INT
+);
 INSERT INTO second_table (id, name, score) VALUES
  (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
