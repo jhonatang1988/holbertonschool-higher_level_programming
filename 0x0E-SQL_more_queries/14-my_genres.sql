f3188c8724161dc52e07c9215dce5d98ec0348ab
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-11-26 22:51:50 +0000

    14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,8 @@
 -- list genres of one show
 SELECT tv_genres.name
 FROM tv_genres
-     INNER JOIN tv_show_genres
+     JOIN tv_show_genres
      ON tv_genres.id = tv_show_genres.genre_id
-     WHERE tv_show_genres.show_id = 8
-     ORDER BY tv_genres.name ASC;
+     JOIN tv_shows
+     ON tv_shows.id = tv_show_genres.show_id
+     WHERE tv_shows.title = 'Dexter';
