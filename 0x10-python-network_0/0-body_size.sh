a56ed4a39625598f60e475112b4bfcfd5c05230d
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-01-11 19:23:58 +0000

    0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # display the size of the response body in bytes
-if [[ $# -ne 1 ]]; then
-    exit 1;
-else
-    res=$(curl -s -I 0.0.0.0:5000 | grep "^Content-Length:" | cut -c17-)
-    echo "$res"
-fi
+curl -s -I 0.0.0.0:5000 | grep "^Content-Length:" | cut -c17-
