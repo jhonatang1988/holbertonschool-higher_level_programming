66b8ba00b9f59f21f3eb073992a0dffb85d92511
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-01-12 21:23:11 +0000

    101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a json post request using curl and get the response body
-curl "$1" -X POST -H "Content-Type: application/json" -d "@$2"
+curl -s "$1" -X POST -H "Content-Type: application/json" -d "@$2"
