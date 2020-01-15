7c333c30c7ca349605b1dc98f8b62a9fd4511ee9
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-01-15 19:12:02 +0000

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,11 +10,11 @@ if __name__ == "__main__":
     try:
         with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
             html = res.read()
+            utfdecode = html.decode('utf-8')
             print("Body response:")
             print("\t- type: {}".format(type(html)))
             print("\t- content: {}".format(html))
             print("\t- utf8 content: {}"
-                  .format('OK' if 'utf-8'
-                  in res.info()['Content-Type'] else 'NO'))
+                  .format(utfdecode))
     except urllib.error.URLError as e:
         print(e.reason)
