e95b48bb875210fd977f7b45c3904f25121bb56a
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-01-15 21:47:10 +0000

    5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+get cystom header from request
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
