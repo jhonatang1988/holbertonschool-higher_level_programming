67914b5a6cfcf462d035e8cd1861eda66b18f8b0
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-01-16 00:22:37 +0000

    10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,9 +9,5 @@ import sys
 if __name__ == "__main__":
     r = requests.get("https://api.github.com/user",
                      auth=(sys.argv[1], sys.argv[2]))
-    if r.status_code == 200:
-        rjson = r.json()
-        if rjson:
-            print(rjson.get('id'))
-        else:
-            print("None")
+    rjson = r.json()
+    print(rjson.get('id'))
