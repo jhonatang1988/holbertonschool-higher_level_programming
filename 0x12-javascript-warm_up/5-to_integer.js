843e228994d17f796337bcdda1318468a9420301
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-02-04 21:13:38 +0000

    5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const convToInt = parseInt(process.argv[2]);
 if (!convToInt || isNaN(convToInt)) {
   console.log('Not a number');
 } else {
-  console.log(convToInt);
+  console.log('My number: ' + convToInt);
 }
