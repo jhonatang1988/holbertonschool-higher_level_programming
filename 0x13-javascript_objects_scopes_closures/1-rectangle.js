96189601cee6313e1c5be4d4f7b85ed1e638adb1
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-02-10 23:08:41 +0000

    1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h){
-	let th = this;
-	th.width = w;
-	th.height = h;
-    }
+  constructor (w, h) {
+    const th = this;
+    th.width = w;
+    th.height = h;
+  }
 }
-module.exports = Rectangle
+module.exports = Rectangle;
