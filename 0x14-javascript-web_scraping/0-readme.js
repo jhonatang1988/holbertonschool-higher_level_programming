5e0cfe6fda5e75906b8a08d0321c5b64b0775e85
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-02-11 19:33:55 +0000

    0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,10 @@ const fs = require('fs');
 
 const fileA = process.argv[2];
 
-try {
-  const data = fs.readFileSync(`./${fileA}`, 'utf8');
+fs.readFile(`${fileA}`, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
   console.log(data);
-} catch (err) {
-  console.log(err);
-}
+});
