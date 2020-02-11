7fb33ab3df471a79fbbc1c913234a617fc7561cf
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-02-11 01:06:30 +0000

    6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,10 +5,17 @@ class Square extends SquareSuper {
 	super(size, size);
     }
     charPrint(c){
-	if(c) {
-	    this.print(c);
-	} else {
-	    this.print('X');
+	if (this.width && this.height) {
+	    for (let i = 0; i < this.height; i++) {
+		for (let j = 0; j < this.width; j++) {
+		    if(c){
+			process.stdout.write(c);
+		    } else {
+			process.stdout.write('X');
+		    }
+		}
+		console.log();
+	    }
 	}
     }
 };
