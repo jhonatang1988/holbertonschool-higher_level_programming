c606698f1b1a5e04e33b0997bdcac7a41017285b
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-02-11 21:50:42 +0000

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,21 +6,17 @@ r(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    if (response.statusCode === 200) {
-      page = '1';
-      while (page) {
-        const jsonbody = JSON.parse(body);
-        page = jsonbody.next;
-        console.log(jsonbody.results.map(function (film, index) {
-          return film.characters.filter(function (character, index) {
-            return character === 'https://swapi.co/api/people/18/';
-          });
-        }).filter(function (movie) {
-          return movie.length > 0;
-        }).length);
-      }
-    } else {
-      console.log('api caida');
+    page = '1';
+    while (page) {
+      const jsonbody = JSON.parse(body);
+      page = jsonbody.next;
+      console.log(parseInt(jsonbody.results.map(function (film, index) {
+        return film.characters.filter(function (character, index) {
+          return character === 'https://swapi.co/api/people/18/';
+        });
+      }).filter(function (movie) {
+        return movie.length > 0;
+      }).length));
     }
   }
 });
