686089e6f591333c74d573a19e4b0f0daf6f461e
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-02-11 21:53:27 +0000

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,17 @@
 #!/usr/bin/node
 const r = require('request');
 const url = process.argv[2];
-let page;
 r(url, function (error, response, body) {
   if (error) {
-    console.log(error);
+    console.error(error);
   } else {
-    page = '1';
-    while (page) {
-      const jsonbody = JSON.parse(body);
-      page = jsonbody.next;
-      console.log(parseInt(jsonbody.results.map(function (film, index) {
-        return film.characters.filter(function (character, index) {
-          return character === 'https://swapi.co/api/people/18/';
-        });
-      }).filter(function (movie) {
-        return movie.length > 0;
-      }).length));
-    }
+    const jsonbody = JSON.parse(body);
+    console.log(parseInt(jsonbody.results.map(function (film, index) {
+      return film.characters.filter(function (character, index) {
+        return character === 'https://swapi.co/api/people/18/';
+      });
+    }).filter(function (movie) {
+      return movie.length > 0;
+    }).length));
   }
 });
