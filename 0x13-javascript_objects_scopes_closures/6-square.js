a88e2060b76c73e72337b900b1dfa2d13c8e1c2c
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-02-11 01:10:23 +0000

    6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,22 +1,23 @@
 #!/usr/bin/node
 const SquareSuper = require('./5-square');
 class Square extends SquareSuper {
-    constructor(size){
-	super(size, size);
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          if (c) {
+            process.stdout.write(c);
+          } else {
+            process.stdout.write('X');
+          }
+        }
+        console.log();
+      }
     }
-    charPrint(c){
-	if (this.width && this.height) {
-	    for (let i = 0; i < this.height; i++) {
-		for (let j = 0; j < this.width; j++) {
-		    if(c){
-			process.stdout.write(c);
-		    } else {
-			process.stdout.write('X');
-		    }
-		}
-		console.log();
-	    }
-	}
-    }
-};
+  }
+}
 module.exports = Square;
