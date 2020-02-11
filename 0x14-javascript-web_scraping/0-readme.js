789a4d61fd7ddcfcea581ffaaf771b90fc7e89b6
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-02-11 19:45:41 +0000

    0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,8 +2,8 @@
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    console.error(err)
-    return
+    console.error(err);
+  } else {
+      console.log(data);
   }
-  console.log(data)
 })
