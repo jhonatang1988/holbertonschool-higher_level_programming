d565b8322dd7fe8ad486f11f8d5cb4d2048a240a
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-02-11 22:59:26 +0000

    6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const r = require('request');
+const url = 'https://jsonplaceholder.typicode.com/todos';
+r(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const todos = JSON.parse(body);
+    const uniqueIds = new Set();
+    const myObj = {};
+
+    for (const todo of todos) {
+      uniqueIds.add(todo.userId);
+    }
+
+    for (const id of uniqueIds) {
+      let counter = 0;
+      myObj[id] = counter;
+      for (const todo of todos) {
+        if (todo.userId === id && todo.completed) {
+          counter++;
+          myObj[id] = counter;
+        }
+      }
+    }
+    console.log(myObj);
+  }
+});
