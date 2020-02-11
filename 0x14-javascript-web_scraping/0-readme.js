8f077e88f6f8b4b505140073d59be52424d6071f
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-02-11 19:37:26 +0000

    0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-const fileA = process.argv[2];
-
-fs.readFile(fileA, 'utf8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
     console.log(data);
   }
