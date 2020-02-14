1b531806a1b6b07738603c7e4641b5074c5f86fa
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-02-14 01:27:57 +0000

    7

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+function DocumentReady () {
+  $.getJSON('https://swapi.co/api/people/5/?format=json',
+    function (data) {
+      $('DIV#character').text(data.name);
+    });
+}
+$(document).ready(DocumentReady);
